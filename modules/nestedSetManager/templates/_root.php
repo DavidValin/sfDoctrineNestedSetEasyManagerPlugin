eb1203ea75d218b2fce111ade83905deeee5b5f4
Binarty <info@binarty.com>
2010-12-27 14:18:41 +0100

    negligence

--- modules/nestedSetManager/templates/_root.php
+++ modules/nestedSetManager/templates/_root.php
@@ -3,8 +3,6 @@
     <?php include_partial('nestedSetManager/row', array('model' => $model, 'field' => $field, 'current_record' => $current_record)) ?>
 
     <?php // If there are children we call this template again to begin a new root template ?>
-    
-    <?php @FIXME: Sustituir getChildren() ?>
     <?php if ($children = $current_record->getNode()->getChildren()): ?>
       <?php foreach ($children AS $child): ?>
         <?php include_partial('nestedSetManager/root', array('model' => $model, 'field' => $field, 'current_record' => $child)) ?>
